c8b9eaf472597b20618408e81d87a946c7083d58
VenturaVini <vini.ventura98@gmail.com>
2026-03-19 16:15:57 -0300

    feat: ajuste de log para salvar em formato json uma vez por dia

--- system/core/config/logger.py
+++ system/core/config/logger.py
@@ -1,5 +1,6 @@
 import logging
 import json
+import os
 from datetime import datetime
 
 
@@ -28,24 +29,35 @@ class JsonFormatter(logging.Formatter):
         return json.dumps(log_data, ensure_ascii=False)
 
 
-def setup_logger(modulo: str) -> logging.Logger:
+def setup_logger(modulo: str, log_dir: str = './system/data/logs') -> logging.Logger:
     '''
     Configura um logger para o módulo com formatação JSON.
+    Salva em arquivo e imprime no console.
 
     Args:
         modulo: nome do módulo (ex: modulo_a, modulo_e)
+        log_dir: pasta onde os logs serão salvos
 
     Returns:
         logger configurado e pronto pra usar
     '''
     logger = logging.getLogger(modulo)
     logger.setLevel(logging.DEBUG)
-    
+
     # handler pra console
-    handler = logging.StreamHandler()
-    handler.setLevel(logging.DEBUG)
-    handler.setFormatter(JsonFormatter())
-    
-    logger.addHandler(handler)
-    
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.DEBUG)
+    console_handler.setFormatter(JsonFormatter())
+    logger.addHandler(console_handler)
+
+    # handler pra arquivo
+    os.makedirs(log_dir, exist_ok=True)
+    data = datetime.now().strftime('%Y%m%d')
+    arquivo_log = os.path.join(log_dir, f'{modulo}_{data}.log')
+
+    file_handler = logging.FileHandler(arquivo_log, encoding='utf-8')
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(JsonFormatter())
+    logger.addHandler(file_handler)
+
     return logger
