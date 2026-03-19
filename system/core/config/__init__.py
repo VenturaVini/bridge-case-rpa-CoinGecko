8215b8990d2af7bd2b048941cfb3b1b3c494ffac
VenturaVini <vini.ventura98@gmail.com>
2026-03-19 17:02:41 -0300

    init: develop branch vazia

--- system/core/config/__init__.py
+++
@@ -1,4 +0,0 @@
-from system.core.config.logger import setup_logger, JsonFormatter
-from system.core.config.config import carregar_config
-
-__all__ = ['setup_logger', 'JsonFormatter', 'carregar_config']
